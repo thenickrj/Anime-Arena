0f2e474ccdf9ce9d69785d31b2ab1258d381ddda
Nikhil Singh <nikhilbangalor@gmail.com>
2021-07-01 03:59:26 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
+      {/* <header className="App-header">
+       */}
+      <img
+        src="https://i.pinimg.com/originals/b9/e5/b4/b9e5b4090dffe0f38880e62350c5b6fe.png"
+        className="App-logo"
+        alt="logo"
+      />
+      {/*<p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
@@ -17,7 +22,19 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+      <h1
+        style={{
+          color: "red",
+          fontFamily: "monospace",
+          fontSize: "50px",
+          opacity: "0.6",
+          left: "40%",
+          // transform: "rotate(90deg)",
+        }}
+      >
+        Welcome to Anime Arena!!
+      </h1>
     </div>
   );
 }
